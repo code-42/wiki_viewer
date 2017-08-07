db64e13bf2ff5f04b8daf461f7972dda6a2ed7af
Edward Dupre <edwd42@gmail.com>
2017-08-07 01:01:57 +0000

    wiki api working and displays title and segment

---
+++ resources/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    console.log("wheres waldo 1");
+    $("#search").on("click", function() {
+        console.log("wheres waldo");
+        var searchTerm = $("#searchTerm").val();
+        console.log(searchTerm);
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&namespace=0&callback=?";
+        console.log(url);
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: "json",
+            success: function (data, status, jqXHR) {
+                var html = "<ul class='list-group'>"
+                for(var i=0; i < data[1].length; i++){
+                    html += 
+                    "<li class='list-group-item'>" +
+                    "<a href='" + data[3][i] + "' target='_blank' class='title'>" + data[1][i] + "</a>" +
+                    "<p>" + data[2][i] + "</p>" +
+                    "</li>"
+                }
+                html += "</ul>";
+                $("#search_results").html(html);
+            }
+        });
+    });
+}); 
